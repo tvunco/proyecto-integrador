527d2d9f6cfb60696bff444e53af8f650ae2cae8
tvunco <tvunco@gmail.com>
2023-03-12 00:04:02 -0300

    creacion de back y front

---
+++ pifer/Back-End/src/main/java/ar/miapp/fer/fernando/FernandoApplication.java
@@ -0,0 +1,13 @@
+package ar.miapp.fer.fernando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandoApplication.class, args);
+	}
+
+}
